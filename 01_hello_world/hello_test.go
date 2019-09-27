ca43d13046be74920a57e931e6885f7b308e1443
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2019-09-27 14:24:46 +0200

    Refactor Hello tests to be table-driven

--- 01_hello_world/hello_test.go
+++ 01_hello_world/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Robin")
-	want := "Hello, Robin!"
-
-	if got != want {
-		t.Errorf("Hello() = %q, want %q", got, want)
+	tests := []struct {
+		name     string
+		receiver string
+		want     string
+	}{
+		{"say hello to a person",
+			"Robin",
+			"Hello, Robin"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Hello(tt.receiver); got != tt.want {
+				t.Errorf("Hello() = %q, want %q", got, tt.want)
+			}
+		})
 	}
 }
