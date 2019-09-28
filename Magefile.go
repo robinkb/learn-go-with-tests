92b4f6f37345b81ef013f7713baa8597e3c05eb8
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2019-09-28 15:41:40 +0200

    Write a bangin' Magefile

--- Magefile.go
+++ Magefile.go
@@ -4,17 +4,104 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 
+	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
-	// mg contains helpful utility functions, like Deps
 )
 
-// Default target to run when none is specified
-// If not set, running mage will list available targets
-// var Default = Build
+// Format all code
+func Fmt() error {
+	return sh.RunV("gofmt", "-s", "-w", ".")
+}
 
-// A build step that requires additional params, or platform specific steps for example
-func Test() error {
-	fmt.Println("Testing...")
-	return sh.RunV("go", "test", "./01_hello_world/...")
+type Test mg.Namespace
+
+// Run all tasks defined under "test"
+func (Test) All() error {
+	typ := reflect.TypeOf(Test{})
+
+	for i := 0; i < typ.NumMethod(); i++ {
+		method := typ.Method(i)
+		if method.Name == "All" { // Lest we recurse infinitely.
+			continue
+		}
+
+		args := []reflect.Value{
+			reflect.ValueOf(Test{}), // The first argument is the receiver.
+		}
+
+		ret := method.Func.Call(args)
+
+		for _, r := range ret {
+			switch v := r.Interface().(type) {
+			case nil:
+			case error:
+				return v
+			default:
+				return fmt.Errorf("unexpected return value %q", r)
+			}
+		}
+	}
+
+	return nil
+}
+
+// Test the "hello" package
+func (Test) Hello() error {
+	return run("hello")
+}
+
+// Test the "integers" package
+func (Test) Integers() error {
+	return run("integers")
+}
+
+// task defines the type for a function that runs a task in the given directory.
+type task func(dir string) error
+
+// run runs a collection of tasks; usually all of them.
+func run(dir string) error {
+	fmt.Printf("Testing %s...\n", dir)
+
+	tasks := []task{
+		format,
+		vet,
+		test,
+	}
+
+	for _, t := range tasks {
+		if err := t(dir); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// format runs 'gofmt' in the given directory, and returns an error when one or more files need formatting.
+func format(dir string) error {
+	output, err := sh.Output("gofmt", "-l", "-s", dir)
+	if err != nil {
+		return err
+	}
+
+	if output != "" {
+		return fmt.Errorf("%s\n%s\n\n%s",
+			"The following files need formatting:",
+			output,
+			"You can fix this problem by running 'mage fmt'.")
+	}
+
+	return nil
+}
+
+// vet runs 'go vet' in the given directory.
+func vet(dir string) error {
+	return sh.RunV("go", "vet", "./"+dir)
+}
+
+// test runs 'go test' in the given directory.
+func test(dir string) error {
+	return sh.RunV("go", "test", "./"+dir)
 }
