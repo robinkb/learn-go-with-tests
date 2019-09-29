a6da0a76664fb0723cf3e456c613ce9cf6727400
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2019-09-29 16:42:26 +0200

    Add alternate implementation of iteration.Repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "strings"
+import (
+	"strings"
+	"unsafe"
+)
 
 func Repeat(s string, count int) string {
 	b := strings.Builder{}
@@ -11,4 +14,18 @@ func Repeat(s string, count int) string {
 	}
 
 	return b.String()
+}
+
+// Alternate implementation based on how strings.Builder works internally.
+// It only has a very small performance advantage over the other function,
+// and it's mostly here because I find the code interesting.
+func Repeat2(s string, count int) string {
+	b := make([]byte, len(s), len(s)*count)
+	copy(b, s)
+
+	for i := 1; i < count; i++ {
+		b = append(b, s...)
+	}
+
+	return *(*string)(unsafe.Pointer(&b))
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -13,7 +13,19 @@ func TestRepeat(t *testing.T) {
 	want := "aaaaaaaa"
 
 	if got := Repeat(args.s, args.count); got != want {
-		t.Errorf("Repeat(%q, %q) = %q, want %q", args.s, args.count, got, want)
+		t.Errorf("Repeat(%q, %d) = %q, want %q", args.s, args.count, got, want)
+	}
+}
+
+func TestRepeatBuf(t *testing.T) {
+	args := struct {
+		s     string
+		count int
+	}{"a", 8}
+	want := "aaaaaaaa"
+
+	if got := Repeat2(args.s, args.count); got != want {
+		t.Errorf("Repeat(%q, %d) = %q, want %q", args.s, args.count, got, want)
 	}
 }
 
@@ -25,7 +37,13 @@ func TestRepeat(t *testing.T) {
 // 	BenchmarkRepeat-8   	10664800	       105 ns/op	      16 B/op	       1 allocs/op
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a", 10)
+		Repeat("a", 100)
+	}
+}
+
+func BenchmarkRepeat2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat2("a", 100)
 	}
 }
 
