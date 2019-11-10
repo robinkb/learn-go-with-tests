2d64b69340bc65298205ba828749a67bec2e949e
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2019-11-10 15:00:49 +0100

    Update property-based-tests package after following the book.

--- property-based-tests/roman_numeral.go
+++ property-based-tests/roman_numeral.go
@@ -1,47 +1,50 @@
 package roman
 
-import (
-	"strings"
-)
+import "strings"
 
-var numerals = map[int]string{
-	1:  "I",
-	5:  "V",
-	10: "X",
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
 }
 
-func Roman(number int) string {
-	if number >= 0 && number <= 3 {
-		return strings.Repeat("I", number)
+var RomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ArabicToRoman(arabic uint16) string {
+	sb := strings.Builder{}
+
+	for _, numeral := range RomanNumerals {
+		for arabic >= numeral.Value {
+			sb.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
 	}
 
-	if number == 4 {
-		return "IV"
+	return sb.String()
+}
+
+func RomanToArabic(roman string) uint16 {
+	var arabic uint16 = 0
+
+	for _, numeral := range RomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			roman = roman[len(numeral.Symbol):]
+			arabic += numeral.Value
+		}
 	}
 
-	if number == 5 {
-		return "V"
-	}
-
-	if number >= 6 && number <= 8 {
-		return "V" + strings.Repeat("I", number-5)
-	}
-
-	if number == 9 {
-		return "IX"
-	}
-
-	if number == 10 {
-		return "X"
-	}
-
-	if number >= 11 && number <= 13 {
-		return "X" + strings.Repeat("I", number-10)
-	}
-
-	if number == 15 {
-		return "XV"
-	}
-
-	return ""
+	return arabic
 }
--- property-based-tests/roman_numeral_test.go
+++ property-based-tests/roman_numeral_test.go
@@ -3,29 +3,78 @@ package roman
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
-func TestRoman(t *testing.T) {
-	cases := []struct {
-		number int
-		want   string
-	}{
-		{1, "I"},
-		{2, "II"},
-		{4, "IV"},
-		{5, "V"},
-		{8, "VIII"},
-		{9, "IX"},
-		{10, "X"},
-		{13, "XIII"},
-		{15, "XV"},
-	}
+var cases = []struct {
+	Arabic uint16
+	Roman  string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{7, "VII"},
+	{8, "VIII"},
+	{9, "IX"},
+	{10, "X"},
+	{14, "XIV"},
+	{18, "XVIII"},
+	{20, "XX"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+	{100, "C"},
+	{90, "XC"},
+	{400, "CD"},
+	{500, "D"},
+	{900, "CM"},
+	{1000, "M"},
+	{1984, "MCMLXXXIV"},
+	{3999, "MMMCMXCIX"},
+	{2014, "MMXIV"},
+	{1006, "MVI"},
+	{798, "DCCXCVIII"},
+}
+
+func TestArabicToRoman(t *testing.T) {
 	for _, test := range cases {
-		name := fmt.Sprintf("%d returns %s", test.number, test.want)
+		name := fmt.Sprintf("%d returns %s", test.Arabic, test.Roman)
 		t.Run(name, func(t *testing.T) {
-			if got := Roman(test.number); got != test.want {
-				t.Errorf("Roman(%d) = %q, want %q", test.number, got, test.want)
+			if got := ArabicToRoman(test.Arabic); got != test.Roman {
+				t.Errorf("ArabicToRoman(%d) = %q, want %q", test.Arabic, got, test.Roman)
 			}
 		})
+	}
+}
+
+func TestRomanToArabic(t *testing.T) {
+	for _, test := range cases {
+		name := fmt.Sprintf("%s returns %d", test.Roman, test.Arabic)
+		t.Run(name, func(t *testing.T) {
+			if got := RomanToArabic(test.Roman); got != test.Arabic {
+				t.Errorf("RomanToArabic(%q) = %d, want %d", test.Roman, got, test.Arabic)
+			}
+		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+
+		roman := ArabicToRoman(arabic)
+		fromRoman := RomanToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
