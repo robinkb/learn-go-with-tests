d2ba22e399c448c1f4d0bf20b4c8804164dc1524
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2024-09-25 11:21:44 +0200

    Format and pick up development again

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -30,11 +30,16 @@ func TestRepeatBuf(t *testing.T) {
 }
 
 // Initial implementation with simple string addition (r += s)
-// 	BenchmarkRepeat-8   	 2539945	       466 ns/op	      64 B/op	       9 allocs/op
+//
+//	BenchmarkRepeat-8   	 2539945	       466 ns/op	      64 B/op	       9 allocs/op
+//
 // Second implementation with slice allocated to wanted capacity
+//
 //	BenchmarkRepeat-8   	 3752137	       320 ns/op	     176 B/op	       2 allocs/op
+//
 // Third implementation with strings.Builder
-// 	BenchmarkRepeat-8   	10664800	       105 ns/op	      16 B/op	       1 allocs/op
+//
+//	BenchmarkRepeat-8   	10664800	       105 ns/op	      16 B/op	       1 allocs/op
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 100)
