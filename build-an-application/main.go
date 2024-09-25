2e03f764be6752cf1d4acd4917923f64486f84c4
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2024-09-25 20:04:29 +0200

    Complete first stage of building an application

---
+++ build-an-application/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/binary"
+	"log"
+	"net/http"
+	"sync"
+
+	badger "github.com/dgraph-io/badger/v4"
+)
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}, sync.Mutex{}}
+}
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+	mu    sync.Mutex
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.store[name]++
+}
+
+func NewBadgerPlayerStore() (*BadgerPlayerStore, error) {
+	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Note that the DB is never closed, it's just toy code.
+
+	return &BadgerPlayerStore{db}, nil
+}
+
+type BadgerPlayerStore struct {
+	db *badger.DB
+}
+
+func (b *BadgerPlayerStore) GetPlayerScore(name string) int {
+	var score int
+
+	err := b.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(name))
+		if err == badger.ErrKeyNotFound {
+			score = 0
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		value, err := item.ValueCopy(nil)
+		if err != nil {
+			return err
+		}
+
+		score = int(binary.BigEndian.Uint16(value))
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return score
+}
+
+func (b *BadgerPlayerStore) RecordWin(name string) {
+	var score int
+
+	err := b.db.Update(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(name))
+		if err != badger.ErrKeyNotFound {
+			value, err := item.ValueCopy(nil)
+			if err != nil {
+				return err
+			}
+
+			score = int(binary.BigEndian.Uint16(value))
+		}
+
+		score++
+		value := make([]byte, 10)
+		binary.BigEndian.PutUint16(value, uint16(score))
+
+		return txn.Set([]byte(name), value)
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	store, err := NewBadgerPlayerStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := &PlayerServer{store}
+	log.Fatal(http.ListenAndServe(":5000", server))
+}
---
+++ build-an-application/server_integration_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store, err := NewBadgerPlayerStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	server := PlayerServer{store}
+	player := "Pepper"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	assertStatus(t, response.Code, http.StatusOK)
+
+	assertResponseBody(t, response.Body.String(), "3")
+}
